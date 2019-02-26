62bd3652eb28844f954b6e0ef4a2be61cf1d8e1d
Daryl Dalan <diieem@outlook.com>
2019-02-26 15:18:59 +1300

    Modified game board

--- TictactoeConsole/TicTacToe.cs
+++ TictactoeConsole/TicTacToe.cs
@@ -2,20 +2,14 @@ namespace TicTacToeConsole
 {
     public class TicTacToe
     {
-        public char[,] Board = new char[3, 3];
+        public Board GameBoard;
         public char CurrentPlayer { get; set; }
         public char Winner { get; set; }
         public bool GameEnded { get; set; }
 
         public TicTacToe()
         {
-            for (int row = 0; row < 3; row++)
-            {
-                for (int column = 0; column < 3; column++)
-                {
-                    Board[row, column] = '.';
-                }
-            }
+            GameBoard = new Board();
 
             CurrentPlayer = 'X';
             Winner = ' ';
