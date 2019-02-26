6e0f3a829cb874660e20a2f860957e3f66dc4a42
Daryl Dalan <diieem@outlook.com>
2019-02-26 15:19:55 +1300

    Removed game board

--- TictactoeConsole/TicTacToe.cs
+++ TictactoeConsole/TicTacToe.cs
@@ -2,15 +2,12 @@ namespace TicTacToeConsole
 {
     public class TicTacToe
     {
-        public Board GameBoard;
         public char CurrentPlayer { get; set; }
         public char Winner { get; set; }
         public bool GameEnded { get; set; }
 
         public TicTacToe()
         {
-            GameBoard = new Board();
-
             CurrentPlayer = 'X';
             Winner = ' ';
         }
